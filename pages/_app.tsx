858f3c3baea57776b45c81675200890495ca7299
Daulet Tulepbergenov <daulet6368@gmail.com>
2022-10-15 00:39:18 +0600

    Add Head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,17 @@ import "@fontsource/noto-sans/600.css";
 import "@fontsource/noto-sans/700.css";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>OWLtop</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default MyApp;
