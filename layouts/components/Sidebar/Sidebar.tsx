00ad6ad32cb2851adb85673ba6eb669f995861a8
Daulet Tulepbergenov <daulet6368@gmail.com>
2022-11-16 20:59:07 +0600

    Add Logo

--- layouts/components/Sidebar/Sidebar.tsx
+++ layouts/components/Sidebar/Sidebar.tsx
@@ -2,10 +2,18 @@ import { SidebarProps } from "./Sidebar.props";
 import styles from "./Sidebar.module.css";
 import cn from "classnames";
 import { Menu } from "../Menu/Menu";
+import { LogoIcon } from "../../../assets/imgs/icons/jsx";
+import Link from "next/link";
 
 export const Sidebar = ({ className, ...props }: SidebarProps) => {
   return (
-    <aside {...props} className={cn(styles.sidebar, className)}>
+    <aside className={cn(styles.sidebar, className)} {...props}>
+      <Link href="/">
+        <a>
+          <LogoIcon className="mb-[19px]" />
+        </a>
+      </Link>
+      <div>Поиск</div>
       <Menu />
     </aside>
   );
