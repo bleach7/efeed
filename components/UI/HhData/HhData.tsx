79c21a7ce5358aefa2d8ca4ef61c386600f8855b
Daulet Tulepbergenov <daulet6368@gmail.com>
2022-11-21 22:18:59 +0600

    Make Hh card

---
+++ components/UI/HhData/HhData.tsx
@@ -0,0 +1,61 @@
+import { FC } from "react";
+import { HhStarIcon } from "../../../assets/imgs/icons/jsx";
+import { Card } from "../Card/Card";
+import styles from "./HhData.module.css";
+import { HhDataProps } from "./HhData.props";
+import cn from "classnames";
+
+export const HhData: FC<HhDataProps> = ({
+  count,
+  middleSalary,
+  juniorSalary,
+  seniorSalary,
+}) => {
+  return (
+    <div className={styles.hh}>
+      <Card className={cn(styles.card, styles.card_count)}>
+        <div className={cn(styles.title, styles.title_count)}>
+          Всего вакансий
+        </div>
+        <div className="text-center text-[36px] font-bold leading-none text-[#7653FC]">
+          {count}
+        </div>
+      </Card>
+      <div className={styles.salary}>
+        <Card className={styles.card}>
+          <div className={styles.title}>Начальный</div>
+          <div className="text-[26px] font-bold leading-none text-[#3B434E]">
+            {juniorSalary} ₽
+          </div>
+          <div className={styles.stars}>
+            <HhStarIcon className={styles.filled} />
+            <HhStarIcon />
+            <HhStarIcon />
+          </div>
+        </Card>
+        <Card className={styles.card}>
+          <div className={styles.title}>Средний</div>
+          <div className="text-[26px] font-bold leading-none text-[#3B434E]">
+            {middleSalary} ₽
+          </div>
+          <div className={styles.stars}>
+            <HhStarIcon className={styles.filled} />
+            <HhStarIcon className={styles.filled} />
+            <HhStarIcon />
+          </div>
+        </Card>
+        <Card className={styles.card}>
+          <div className={styles.title}>Профессионал</div>
+          <div className="text-[26px] font-bold leading-none text-[#3B434E]">
+            {seniorSalary} ₽
+          </div>
+          <div className={styles.stars}>
+            <HhStarIcon className={styles.filled} />
+            <HhStarIcon className={styles.filled} />
+            <HhStarIcon className={styles.filled} />
+          </div>
+        </Card>
+      </div>
+    </div>
+  );
+};
--- components/UI/index.ts
+++ components/UI/index.ts
@@ -3,3 +3,5 @@ export * from "./Text/Text";
 export * from "./Badge/Badge";
 export * from "./Button/Button";
 export * from "./Rating/Rating";
+export * from "./Card/Card";
+export * from "./HhData/HhData";
