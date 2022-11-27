020046eac73406d092066f07400c5ec7364e24b7
Daulet Tulepbergenov <daulet6368@gmail.com>
2022-11-27 22:35:47 +0600

    Add Advantages, Sorting

--- components/pages/TopPageComponent/TopPageComponent.tsx
+++ components/pages/TopPageComponent/TopPageComponent.tsx
@@ -1,37 +1,83 @@
-import { FC } from "react";
+import { FC, useReducer } from "react";
 import { TopLevelCategory } from "../../../interfaces/page.interface";
-import { Badge, Heading, HhData } from "../../UI";
+import { Advantages } from "../../Advantages/Advantages";
+import { Badge, Heading } from "../../UI";
 import { TopPageComponentProps } from "./TopPageComponent.props";
+import styles from "./TopPageComponent.module.css";
+import { HhData } from "../../HhData/HhData";
+import { Sorting } from "../../Sorting/Sorting";
+import { SortEnum } from "../../Sorting/Sorting.props";
+import { sortReducer } from "./sort.reducer";
 
 export const TopPageComponent: FC<TopPageComponentProps> = ({
   firstCategory,
   page,
   products,
 }) => {
+  const [{ products: sortedProducts, sort }, dispatchSort] = useReducer(
+    sortReducer,
+    {
+      products,
+      sort: SortEnum.Rating,
+    }
+  );
+
+  const setSort = (sort: SortEnum): void => {
+    dispatchSort({ type: sort });
+  };
+
   return (
     <section className="pt-[35px]">
-      <div className="grid grid-cols-[auto_1fr_auto] items-center justify-items-start gap-[20px]">
+      <div className="mb-[20px] grid grid-cols-[auto_1fr_auto] items-center justify-items-start gap-[20px]">
         <Heading as="h1">{page.title}</Heading>
         {Boolean(products) && (
           <Badge appearance="gray" size="md">
             {products.length}
           </Badge>
         )}
-        <span>Сортировка</span>
+        <Sorting sort={sort} setSort={setSort} />
       </div>
       <div>
-        {Boolean(products) &&
-          products.map((product) => (
+        {Boolean(sortedProducts) &&
+          sortedProducts.map((product) => (
             <article key={product._id}>{product.title}</article>
           ))}
       </div>
-      <div className="grid grid-cols-[auto_1fr] items-center justify-items-start gap-[20px]">
+      <div className="mb-[25px] mt-[51px] grid grid-cols-[auto_1fr] items-center justify-items-start gap-[20px]">
         <Heading as="h2">Вакансии - {page.category}</Heading>
         <Badge appearance="red" size="md">
           hh.ru
         </Badge>
       </div>
-      {firstCategory === TopLevelCategory.Courses && <HhData {...page.hh} />}
+      {firstCategory === TopLevelCategory.Courses && page.hh && (
+        <HhData {...page.hh} />
+      )}
+      {page.advantages && page.advantages.length > 0 && (
+        <>
+          <Heading as="h2" className="mt-[47px] mb-[27px]">
+            Преимущества
+          </Heading>
+          <ul className="mb-[30px]">
+            <Advantages advantages={page.advantages} />
+          </ul>
+        </>
+      )}
+      {page.seoText && (
+        <div
+          className={styles.seo}
+          dangerouslySetInnerHTML={{ __html: page.seoText }}
+        ></div>
+      )}
+      <Heading as="h2" className="mt-[30px] mb-[20px]">
+        Получаемые навыки
+      </Heading>
+      <div className="flex items-center">
+        {page.tags.map((tag) => (
+          <Badge size="sm" appearance="primary" key={tag}>
+            {tag}
+          </Badge>
+        ))}
+      </div>
     </section>
   );
 };
